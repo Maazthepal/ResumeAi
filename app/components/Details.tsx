37867253ec7f9d8d58c46ad621a65b416abc79e9
maaz <maazasif587@gmail.com>
2025-08-01 23:41:26 +0500

    fixing Build error

--- app/components/Details.tsx
+++ app/components/Details.tsx
@@ -4,7 +4,7 @@ import {
   AccordionContent,
   AccordionHeader,
   AccordionItem,
-} from "./accordion";
+} from "./Accordion";
 
 const ScoreBadge = ({ score }: { score: number }) => {
   return (
